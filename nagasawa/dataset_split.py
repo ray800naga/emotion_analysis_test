e497df7d49306a2d872268cf3a0148a6695e2a68
Naomu Nagasawa <naga.lcw.ld.0203@keio.jp>
2022-11-08 01:37:23 +0900

    success to run training

--- nagasawa/dataset_split.py
+++ nagasawa/dataset_split.py
@@ -1,23 +1,17 @@
-SRC_FILE_NAME = '/workspace/emotion_analysis_test/nagasawa/data_src/wiki40b_with_emotion/wiki_40b_train_with_emotion.txt'
-DST_FILE_NAME_HEAD = '/workspace/emotion_analysis_test/nagasawa/data_src/BERT_to_emotion/train/split/split_wiki_40b_train_with_emotion_'
+MODE = 'test'
+SRC_FILE_NAME = '/workspace/dataset/data_src/BERT_to_emotion/only_emotion/{0}/BERT_to_emo_{0}.txt'.format(MODE)
+DST_FILE_NAME_HEAD = '/workspace/dataset/data_src/BERT_to_emotion/only_emotion/{0}/split/split_wiki_40b_{0}_with_emotion_'.format(MODE)
 
-def write_file(line_list, file_num):
-	with open(DST_FILE_NAME_HEAD + '{:0>3}.txt'.format(file_num), 'w') as f:
-		for line in line_list:
-			f.write(line)
+def write_file(line, file_num):
+	with open(DST_FILE_NAME_HEAD + '{:0>8}.txt'.format(file_num), 'w') as f:
+		f.write(line)
 
 with open(SRC_FILE_NAME, 'r') as f:
-	line_list = []
 	count = 0
 	file_num = 1
 	for line in f:
-		line_list.append(line)
 		count += 1
-		if count % 150000 == 0:
-			print("write:{}".format(file_num))
-			write_file(line_list, file_num)
-			file_num += 1
-			line_list = []
-	print("write:{}".format(file_num))
-	write_file(line_list, file_num)
+		print("write:{}".format(file_num))
+		write_file(line, file_num)
+		file_num += 1
 

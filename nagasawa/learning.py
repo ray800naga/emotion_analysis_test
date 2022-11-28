13ae916767bbfaf7e3dc846cd1dd1cc190344111
Naomu Nagasawa <naga.lcw.ld.0203@keio.jp>
2022-11-28 16:16:10 +0900

    learning change squeeze

--- nagasawa/learning.py
+++ nagasawa/learning.py
@@ -55,9 +55,9 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = self.fc1(x)
-        x = self.bn(x)
-        # x = F.relu(x)
-        x = F.leaky_relu(x)
+        # x = self.bn(x)
+        x = F.relu(x)
+        # x = F.leaky_relu(x)
         x = self.fc2(x)
         # x = F.relu(x)
         # x = self.fc3(x)
@@ -104,8 +104,8 @@ for epoch in range(max_epoch):
     loss_list = []
     for batch in tqdm(train_file_loader):
         x, t = batch
-        x = x.view(-1, 768)
-        t = t.view(-1, 10)
+        x = x.squeeze()
+        t = t.squeeze()
         x = x.to(device)
         t = t.to(device)
 
